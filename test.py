ad9d93d496c4f779f6992a09872aef6b9e81b91e
marjajaa <marja.k.jaatinen@gmail.com>
2026-04-16 17:51:01 +0300

    update ja delete, eka css versio

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -19,22 +19,52 @@ class Recipe(db.Model):
     def __repr__(self):
         return f"<Recipe {self.name}>"
     
+@app.route("/delete/<int:id>", methods=["POST"])
+def delete_recipe(id):
+    recipe_to_delete = Recipe.query.get(id)
+
+    try:
+        db.session.delete(recipe_to_delete)
+        db.session.commit()
+        return redirect("/")
+    except:
+        return "Error deleting recipe"
+
+    
 @app.route("/add", methods=["POST"])
-def add():
+def add_recipe():
     recipe = Recipe(
         name = request.form["name"],
         ingredients = request.form["ingredients"],
         instructions = request.form["instructions"],
-        time = request.form["time"]
+        time = int(request.form["time"])
         )
         
     db.session.add(recipe)
     db.session.commit()
-    return "Added"
+    return redirect("/")
 
-@app.route('/')
+@app.route("/edit/<int:id>")
+def edit_recipe(id):
+    recipe = Recipe.query.get(id)
+    return render_template("edit.html", recipe=recipe)
+
+@app.route("/update/<int:id>", methods=["POST"])
+def update_recipe(id):
+    recipe = Recipe.query.get(id)
+
+    recipe.name = request.form["name"]
+    recipe.ingredients = request.form["ingredients"]
+    recipe.instructions = request.form["instructions"]
+    recipe.time = int(request.form["time"])
+
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/", methods=["GET"])
 def index():
-    return render_template('index.html')
+    recipes = Recipe.query.all()
+    return render_template("index.html", recipes=recipes)
 
 if __name__ == "__main__":
     with app.app_context():
